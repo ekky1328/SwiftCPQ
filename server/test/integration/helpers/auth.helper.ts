0a5b122b0d0677b477a1478e0b7142cb1f154135
ctalke <chris@talke.dev>
2026-03-29 17:17:34 +1100

    fix: replace uuid with crypto's randomUUID in auth.helper.ts and remove uuid from package.json

--- server/test/integration/helpers/auth.helper.ts
+++ server/test/integration/helpers/auth.helper.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID as uuidv4 } from 'crypto';
 import type { Application } from 'express';
 import request from 'supertest';
 import db from '../../../src/database/db';
